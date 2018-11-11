846a7f208dcabb44b50705766f726e123b7e0ee8
Cristopher Castro <cristophernic@live.cl>
2018-11-11 15:04:23 -0300

    .

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -512,7 +512,7 @@
                     if (response.profesional_userid == ""){
                         $("#dialog\\.delete").remove();
                         $("#dialog\\.title").html('Asociar una cuenta con un médico');
-                        $("#dialog\\.body").html('<div class="row"><div class="col-12"><p class="text-center">El administrador ha ingresado los siguientes médicos a la plataforma, seleccione su nombre y presione continuar.</p></p><div class="form-group col-12"><label for="turnos.profesionales">Profesional</label><select class="form-control" id="turnos.profesionales"></select></div></div>');
+                        $("#dialog\\.body").html('<div class="row"><div class="col-12"><p class="text-center">El administrador ha ingresado los siguientes médicos a la plataforma, seleccione su nombre y presione continuar.</p></p><div class="form-group col-12"><label for="turnos.profesionales" class="text-center text-danger">Profesional</label><select class="form-control" id="turnos.profesionales"></select></div></div>');
                         $("#dialog\\.footer").append('<button type="button" class="btn btn-danger" id="dialog.delete" data-id="">Continuar</button>');
                         $("#dialog\\.view").modal("show");
                         cargarProfesionales();
@@ -521,7 +521,7 @@
                 else{
                     $("#dialog\\.delete").remove();
                     $("#dialog\\.title").html('Asociar una cuenta con un médico');
-                    $("#dialog\\.body").html('<div class="row"><div class="col-12"><p class="text-center">El administrador ha ingresado los siguientes médicos a la plataforma, seleccione su nombre y presione continuar.</p></p><div class="form-group col-12"><label for="turnos.profesionales">Profesional</label><select class="form-control" id="turnos.profesionales"></select></div></div>');
+                    $("#dialog\\.body").html('<div class="row"><div class="col-12"><p class="text-center">El administrador ha ingresado los siguientes médicos a la plataforma, seleccione su nombre y presione continuar.</p></p><div class="form-group col-12"><label for="turnos.profesionales" class="text-center text-danger">Profesional</label><select class="form-control" id="turnos.profesionales"></select></div></div>');
                     $("#dialog\\.footer").append('<button type="button" class="btn btn-danger" id="dialog.delete" data-id="">Continuar</button>');
                     $("#dialog\\.view").modal("show");
                     cargarProfesionales();
