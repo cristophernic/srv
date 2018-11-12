bd603fd671bd0241853266d1fa87535ae63621d5
Cristopher Castro <cristophernic@live.cl>
2018-11-12 15:29:07 -0300

    .

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -10,11 +10,21 @@
                         <?php if (Session::get("user_account_type") == 6) : ?>
                             <li class="nav-item"><a class="nav-link" href="#" id="boton.turno">Ingresar datos</a></li>
                             <li class="nav-item"><a class="nav-link" href="#" id="boton.profesionales">Asignar turnos</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <?php echo Session::get('user_name'); ?>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
                         <?php endif; ?>
                         <li class="nav-item"><a class="nav-link" href="#" id="boton.pormes">Ver turnos por mes</a></li>
                         <li class="nav-item"><a class="nav-link" href="login/logout">Salir del programa</a></li>
                     </ul>
-                    <p class="navbar-text"><?php echo Session::get('user_name'); ?></p>
                 </div>
             </nav>
             <div class="card">
