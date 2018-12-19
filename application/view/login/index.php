1a2054e3cecce3da66f1cad72997455b8deeeae8
Cristopher Castro <cristophernic@live.cl>
2018-12-19 20:47:12 -0300

    .

--- application/view/login/index.php
+++ application/view/login/index.php
@@ -97,8 +97,9 @@
             $("#form\\.registrarse").submit(function( event ) {
                 if ($( "input[name='user_name']" ).val() == ""){
                     $("#registro\\.mensaje").text( "Ingrese un nombre de usuario" ).show().fadeOut( 1000 );
-                    $( "input[name='user_name']" ).addClass("is-invalid");
+                    $( "input[name='user_name']" ).parent().append('<div class="invalid-feedback">Ingrese un nombre de usuario, sin espacios</div>');
                     event.preventDefault();
+                    return;
                 }
                 if ($( "input[name='user_password_new']" ).val() == ""){
                     $("#registro\\.mensaje").text( "Ingrese una contraseña" ).show().fadeOut( 1000 );
