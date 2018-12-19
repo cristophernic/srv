b86364227a4defc461e399f59bba433060bd55eb
Cristopher Castro <cristophernic@live.cl>
2018-12-19 16:08:02 -0300

    .

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -162,9 +162,8 @@
             <?php if (Session::get("user_account_type") == 6) : ?>
             $("#boton\\.turno").on("click", function(){
                 $("#dialog\\.title").html("INGRESAR DATOS (fecha, horario y profesional de turno)");
-                $("#dialog\\.body").html('<div class="row"><div class="form-group col"><label for="turnos.fecha.in">Fecha de turno</label><input class="form-control" type="date" id="turnos.fecha.in"></div><div class="form-group col"><label for="turnos.hora.in">Horario de turno (12 o 24 hrs)</label><select class="form-control" id="turnos.turno"><option value="0">Diurno</option><option value="1">Nocturno</option><option value="2">Completo</option></select></div></div><div class="row"><div class="form-group col"><label for="turnos.profesionales">Profesional asignado</label><select class="form-control" id="turnos.profesionales"></select></div></div>');
+                $("#dialog\\.body").html('<div class="row"> <div class="form-group col-6"><label for="turnos.departamento">Departamento</label><input class="form-control" type="text" id="turnos.departamento"></div><div class="form-group col-6"><label for="turnos.fecha.in">Fecha de turno</label><input class="form-control" type="date" id="turnos.fecha.in"></div><div class="form-group col-6"> <label for="turnos.hora.in">Horario de turno (12 o 24 hrs)</label> <select class="form-control" id="turnos.turno"> <option value="0">Diurno</option> <option value="1">Nocturno</option> <option value="2">Completo</option> </select> </div><div class="form-group col-6"><label for="turnos.profesionales">Profesional asignado</label><select class="form-control" id="turnos.profesionales"></select></div></div>');
                 $("#dialog\\.view").modal("show");
-
 
                 let data = {
                     accion : "profesionalesFiltrados",
