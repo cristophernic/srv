d08a5801ca4da5440adb8c815c391b3733d23d4e
Cristopher Castro <cristophernic@live.cl>
2019-01-02 21:33:29 -0300

    .

--- application/model/TurnosModel.php
+++ application/model/TurnosModel.php
@@ -49,8 +49,8 @@ class TurnosModel
             $semana_fin = "0". strval($semana_fin);
         }
 
-        $fecha1 = strval($ano) . "-" . strval($mes) . "-". strval($semana_ini);
-        $fecha2 = strval($ano) . "-" . strval($mes) . "-". strval($semana_fin);
+        $fecha1 = strval($ano) . '-' . strval($mes) . '-'. strval($semana_ini);
+        $fecha2 = strval($ano) . '-' . strval($mes) . '-'. strval($semana_fin);
 
         $sql = "SELECT turnos.turno_id, turnos.turno_departamento, turnos.turno_profesional, turnos.turno_fechain, turnos.turno_turno, users.user_nombre FROM turnos INNER JOIN users ON turnos.turno_profesional = users.user_id WHERE turnos.turno_departamento = :departamento AND turnos.turno_fechain BETWEEN :turno_fechain AND :turno_fechaout";
         $query = $database->prepare($sql);
